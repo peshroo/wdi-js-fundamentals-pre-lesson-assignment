3502e1d18daf5a18d2d3dc5c141673e1df110259
Roopesh <rrattan.89@gmail.com>
2017-06-29 18:03:23 -0400

    collections of arrays + objects, as well as loops

---
+++ pacman.rb
@@ -0,0 +1,15 @@
+ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde']
+puts "There are #{ghosts.length} ghosts."
+puts 'Their names are: '
+puts ghosts[0]
+puts ghosts[1]
+puts ghosts[2]
+puts ghosts[3]
+
+class Ghost
+  attr_accessor :colour, :personality
+end
+inky = Ghost.new
+inky.colour = 'Cyan'
+inky.personality = 'Shadow'
+puts inky.inspect
