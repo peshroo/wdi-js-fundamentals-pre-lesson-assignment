8b79e53707494984a512fb5eeee227f5b87a594d
Roopesh <rrattan.89@gmail.com>
2017-06-29 18:14:40 -0400

    conditionals -if..else and switch statements

--- pacman.rb
+++ pacman.rb
@@ -13,3 +13,34 @@ inky = Ghost.new
 inky.colour = 'Cyan'
 inky.personality = 'Shadow'
 puts inky.inspect
+
+power_pellet_eaten = false
+ghosts = 4
+while true do
+  puts "Ghosts remaining: #{ghosts}"
+
+  if power_pellet_eaten == false
+    puts 'Pac-Man eats the power pellet.'
+    power_pellet_eaten = true
+  elsif ghosts > 0
+    puts 'Pac-Man eats a ghost.'
+    ghosts--
+  else
+    break
+  end
+end
+
+puts 'Pac-Man goes home after a long day in the maze'
+
+ghost = 'Pinky'
+case ghost
+when 'Inky'
+  colour = 'Cyan'
+when 'Blinky'
+  colour = 'Red'
+when 'Pinky'
+  colour = 'Pink'
+when 'Clyde'
+  colour = 'Orange'
+end
+puts "#{ghost} is the colour #{colour}"
